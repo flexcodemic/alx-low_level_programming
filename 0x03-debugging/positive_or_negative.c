6428885b5003c074bcf9662cb08ae999ff6739e1
Anthony Aruma <anthonyaruma091@gmail.com>
2023-07-20 17:25:31 -0700

    Test the function for zero

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,17 +1,23 @@
 #include "main.h"
 /**
-*positive_or_negative - This file must test that the function
-*0: is the number to be checked
+*positive_or_negative - Check if a number is positive, negative or zero
+*@i: The character check on ASCII code
 *Return: Always 0 (Success)
 */
 void positive_or_negative(int i)
 {
 	if (i > 0)
+	{
 		printf("%d is positive\n", i);
-
+	}
 	else if (i < 0)
+	{
 		printf("%d is negative\n", i);
-
+	}
 	else
+	{
 		printf("%d is zero\n", i);
+	}
+	return;
 }
+
