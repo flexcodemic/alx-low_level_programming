c01b53c263f533fe7753427f8be7a401d65f16d7
Anthony Aruma <anthonyaruma091@gmail.com>
2023-07-25 19:35:15 -0700

    Write a function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,14 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
-*print_array - prints n elements of an array of integers
-*
-*@a: Input array
-*@n: Length of the array
-*
-*Return: Always 0
-*/
+ * print_array - prints n elements of an array of integers
+ *
+ * @a: Input array
+ * @n: Length of the array
+ *
+ * Return: Always 0
+ */
 void print_array(int *a, int n)
 {
 	int arr;
@@ -14,7 +16,7 @@ void print_array(int *a, int n)
 	for (arr = 0; arr < n; arr++)
 	{
 		printf("%d", a[arr]);
-		if (arr != (n + 1))
+		if (arr != (n - 1))
 		{
 			printf(",");
 		}
