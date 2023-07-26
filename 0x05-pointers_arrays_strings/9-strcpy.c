383a7b3affaaf58d342dc43695af32e7306a0caf
Anthony Aruma <anthonyaruma091@gmail.com>
2023-07-25 19:54:02 -0700

    Write a function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+**_strcpy - copies the string pointed to by src,
+*including the terminating null byte (\0),
+*to the buffer pointed to by dest
+*
+*@dest: This is char type string
+*@src: This also char type string
+*
+*Return: pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int w = -1;
+
+	do {
+		w++;
+		dest[w] = src[w];
+	} while (src[w] != '\0');
+	return (dest);
+}
