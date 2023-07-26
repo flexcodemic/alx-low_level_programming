1c8d67c808149d206e61c217a7e8da4f035caf33
Anthony Aruma <anthonyaruma091@gmail.com>
2023-07-26 14:11:46 -0700

    Write a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
- * rot13 - a function that encodes a string using rot13
- *
- * @s: This is the pointer to the string
- *
- * Return: pointer to the encoded string
- */
+**rot13 - a function that encodes a string using rot13
+*
+*@s: This is the pointer to the string
+*
+*Return: pointer string
+*/
 char *rot13(char *s)
 {
 	int i, j;
@@ -14,30 +13,15 @@ char *rot13(char *s)
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char dataRot[] =
 		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
+		for (j = 0; j < 52; j++)
 		{
-			for (j = 0; j < 26; j++)
-			{
-				if (s[i] == myData[j])
-				{
-					s[i] = dataRot[j];
-					break;
-				}
-			}
+		if (s[i] == myData[j])
+		{
+			s[i] = dataRot[j];
+			break;
 		}
-		else if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			for (j = 26; j < 52; j++)
-			{
-				if (s[i] == myData[j])
-				{
-					s[i] = dataRot[j];
-					break;
-				}
-			}
 		}
 	}
 	return (s);
