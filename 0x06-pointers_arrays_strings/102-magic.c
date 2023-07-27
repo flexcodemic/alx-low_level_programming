e44cecb1b3ac5534023d87a34045b8d6a80ee8d3
Anthony Aruma <anthonyaruma091@gmail.com>
2023-07-26 17:18:59 -0700

    Add one line to this code, so that the program prints

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+/*Complete your line of code here
+*You are not allowed to use the variable a in your new line of code
+*You are not allowed to modify the variable p
+*You can only write one statement
+*You are not allowed to use
+*You are not allowed to code anything else
+*than the line of expected line of code
+*/
+	*(p + 5) = 98;
+/* This will print 98\n */
+	printf("*a[2] = %d\n", a[2]);
+	return (0);
+}
