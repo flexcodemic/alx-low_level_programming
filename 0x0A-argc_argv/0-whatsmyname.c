cffb6610abcfef7ef059ffb390bbcc355f2ab3a4
Anthony Aruma <anthonyaruma091@gmail.com>
2023-08-06 12:28:00 -0700

    Write a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main -  a program that prints its name
+*
+*@argc: argument counter
+*@argv: array argument for command line element
+*
+*Return: Always 0 (Success)
+*/
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A. C - argc, argv
+
+## This Project is all about Arguments used command lines
