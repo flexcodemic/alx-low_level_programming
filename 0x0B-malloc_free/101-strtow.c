f92e616882d90005d3e2f64051e239a35b2eb3e0
Anthony Aruma <anthonyaruma091@gmail.com>
2023-08-11 05:01:59 -0700

    Write a function that splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,93 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
+/**
+*word_len - locate the marking index at the end of
+*first word
+*@str: the string to be searched
+*
+*Return: returns a pointer and index marking the end
+*of the string
+*/
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
+	return (len);
+}
+/**
+*count_words - Counts the number of words caontained in a string
+*@str: the string to be searched
+*
+*Return: number words contain in the string
+*/
+int count_words(char *str)
+{
+	int index = 0, words = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+	return (words);
+}
+
+/**
+* strtow - splits a string into words
+*
+*@str: the strint to be splitted
+*
+*Return: if str is equal to NULL, then str = ""
+*otherwise a pointer to an array of strings (words)
+*/
+char **strtow(char *str)
+{
+	char **strings;
+	int index = 0, words = 0, w, letters, l;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+			free(strings);
+			return (NULL);
+		}
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+		strings[w][l] = '\0';
+	}
+	strings[w] = NULL;
+	return (strings);
+}
