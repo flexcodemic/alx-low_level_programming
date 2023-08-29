285d52916563e01675e4b86144dff6c4d535d941
Anthony Aruma <anthonyaruma091@gmail.com>
2023-08-29 12:18:15 -0700

    Write a function that frees a listint_t lis

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,13 @@
 #include "lists.h"
+
 /**
-*free_listint2 - frees a listint_t list
+* free_listint2 - frees a listint_t list
 *
-*@head: pointer to the next node
+* @head: pointer to the next node
 *
-*Return: Nothing
+* Return: Nothing
 */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *free_me;
