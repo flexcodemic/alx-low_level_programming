6d5aa8a613f516f97a5733bc4e85a988bfee4b75
Anthony Aruma <anthonyaruma091@gmail.com>
2023-09-29 03:45:15 -0700

    Write a function that checks the endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@ int _putchar(char c);
 int clear_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
+int get_endianness(void);
 void print_binary(unsigned long int n);
 int set_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
