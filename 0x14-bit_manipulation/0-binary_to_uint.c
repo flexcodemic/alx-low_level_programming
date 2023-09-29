019513d3da5989b96a5af2782e6be1874036e47f
Anthony Aruma <anthonyaruma091@gmail.com>
2023-09-29 01:22:05 -0700

    Write a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,32 +13,32 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int length;
-        size_t result;
-        size_t power;
-        int basement;
+	size_t result;
+	size_t power;
+	int basement;
 	size_t i = 0, j = 0;
 
-        result = 0;
-        basement = 2;
-        power = 1;
+	result = 0;
+	basement = 2;
+	power = 1;
 
-        if (b == NULL)
+	if (b == NULL)
 	{
-                return (0);
+		return (0);
 	}
-        for (length = 0; b[length] != '\0'; length++)
-                ;
-        if (length == 1 && (b[0] == '0' || b[0] == '1'))
-                        return (b[0] - 48);
-        for (i = 0; b[i] != '\0'; i++)
-        {
-                if (b[i] != '0' && b[i] != '1')
-                return (0);
-                for (j = length - 1; j > 0; j--)
-                power = power * basement;
-                result = result +(power * (b[i] - 48));
-                length--;
-                power = 1;
-        }
-        return (result);
+	for (length = 0; b[length] != '\0'; length++)
+		;
+	if (length == 1 && (b[0] == '0' || b[0] == '1'))
+		return (b[0] - 48);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		for (j = length - 1; j > 0; j--)
+			power = power * basement;
+		result = result + (power * (b[i] - 48));
+		length--;
+		power = 1;
+	}
+	return (result);
 }
